bcc7c027a928b7b3854dce0b70d0f4cae4c89f64
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2023-04-14 09:11:25 -0700

    a

--- main.js
+++ main.js
@@ -668,13 +668,7 @@ client.on("messageCreate", async (msg) => {
         `**Team 1**: ${captains[0] ? ":crown:\n " + captains[0] + "\n" : ""}`,
         `${team1 ? team1.join("\n ") + "\n" : ""}`,
         "\n",
-        `**Team 2**: ${
-          captains[1]
-            ? ` :crown:\n ${
-                captains[1] ? captains[1] + `${team2 ? "\n" : ""}` : ""
-              }`
-            : ""
-        }`,
+        `**Team 2**: ${captains[1] ? ":crown:\n " + captains[1] + "\n" : ""}`,
         `${team2 ? team2.join("\n ") + "\n" : ""}`,
         `\n **Draft List**:\n ${inDraft.join(`${"\n"} ${dashSymbol}`)}`,
       ];
