eb085c1482ec751dbffb92068a88e3ef965e9c46
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2023-04-14 00:48:02 -0700

    a

--- main.js
+++ main.js
@@ -288,7 +288,7 @@ client.on("messageCreate", async (msg) => {
     const checkListForMedals = (name) => {
       name = name !== `<@${name}>` ? `<@${name}>` : name;
       const currentPlayer = allServerUsers.find(
-        (user) => user.userId === `<@${name}>` && user.guildId === msg.guild.id
+        (user) => user.userId === name && user.guildId === msg.guild.id
       );
       let newList = allServerUsers.sort((a, b) => b.lp - a.lp);
       newList.filter((a) => a.playedSeason);
