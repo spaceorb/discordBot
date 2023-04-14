9a08b414386192d0e42fb9caad61865adc75dc1d
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2023-04-14 09:30:15 -0700

    a

--- main.js
+++ main.js
@@ -1635,27 +1635,7 @@ client.on("messageCreate", async (msg) => {
             }
 
             if (randomizedAlready === 1) {
-              randomizedArr = [
-                peopleSymbol,
-                `**${team1.length + team2.length + inDraft.length}**`,
-                "\n",
-                dashSymbol,
-
-                "\n",
-                "\n",
-                "**Team 1**:",
-                " ",
-                `${team1.join("\n ")}`,
-                "\n",
-                "\n",
-                "**Team 2**:",
-                " ",
-                `${team2.join("\n ")}`,
-                "\n",
-                `\n **Draft List**:\n ${inDraft.join(`${"\n"} ${dashSymbol}`)}`,
-
-                `\n **Draft List**:\n ${inDraft.join(`${"\n"} ${dashSymbol}`)}`,
-              ];
+              updatePlayerCount();
               removeOldMsg(msg, randomizedArr.join(" "));
             } else {
               removeOldMsg(msg, listArr.join(" "));
