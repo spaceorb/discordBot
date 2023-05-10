1670515bd585dfaba93c5333f11b8bed57999f3a
Kenneth Lee <84689781+spaceorb@users.noreply.github.com>
2023-05-10 16:46:53 -0700

    Delete DraftNumSchema.js

--- models/DraftNumSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DraftNumSchema = new mongoose.Schema({
-  draftNum: { type: Number },
-});
-
-const draftNum = mongoose.model("draftNumModel", DraftNumSchema);
-
-module.exports = draftNum;
